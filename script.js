f75593c3df37a652f0cd36db7c08d50e3aa73d00
djanedsouza <donitadsouza1996@gmail.com>
2019-07-10 13:26:24 +0530

    added and edited files

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log(123);
+var i = 0;
+
+//executes the function for the specified amount of time
+
+/*
+var a = setInterval(function() {
+    if (i==5)
+    {
+        clearInterval(a);
+    }
+    console.log(++i);
+}, 1000);
+*/
+
+//execute only once after the specified amount of time
+
+/*
+setTimeout(function(){
+    console.log("Its 3 seconds");
+},3000);
+*/
+
+/*
+var a = "VSCode is from microsoft microsoft Microsoft";
+var b = a.replace(/microsoft/g, "Google");
+console.log(b);
+*/
+//i - case sensitive flag
+//g - global search flag
